e6e54ba694fa6239d90e4d0a9225e36e0d11fe15
Valentin Richter <valentinsrichter@gmail.com>
2022-04-04 02:19:20 -0400

    add [all] default option and README instructions on bootstrap usage

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # csgocfg
-Place .cfg files in:
-```.\Steam\userdata\[SteamFriendCode]\730\local\cfg``` where [SteamFriendCode] is a 9-digit token found in a Steam browser under Friends->Add a Friend.
+
+If you have Steam and CSGO installed into their default directories, you can use the bootstrap:
+
+```
+powershell -ExecutionPolicy ByPass -File .\bootstrap.ps1 -u [all/SteamFriendCode] -pano [on/off]
+```
+
+By default, [all] users on the computer will have their config files replaced and the panorama background replacement is [off].
+
+#
+
+Place .cfg files in: ```.\Steam\userdata\[SteamFriendCode]\730\local\cfg``` where [SteamFriendCode] is a 9-digit token found in a Steam browser under Friends->Add a Friend.
--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,12 +1,27 @@
 param ($u='all', $pano='off')
 
-if($u -eq 'all'){
-	
+if(Test-Path -Path 'C:\Program Files (x86)\Steam'){
+	if($u -eq 'all'){
+		Get-ChildItem -Path 'C:\Program Files (x86)\Steam\userdata' | 
+		Foreach-Object{
+			if(Test-Path -Path "C:\Program Files (x86)\Steam\userdata\$_\730\local\cfg"){
+				Copy-Item -Filter *.cfg -Path '.\cfg\*' -Recurse -Destination "C:\Program Files (x86)\Steam\userdata\$_\730\local\cfg"
+			}
+		}			
+	}
+	else{
+		Copy-Item -Filter *.cfg -Path '.\cfg\*' -Recurse -Destination "C:\Program Files (x86)\Steam\userdata\$u\730\local\cfg"
+	}
 }
 else{
-	Copy-Item -Filter *.cfg -Path '.\cfg\*' -Recurse -Destination "C:\Program Files (x86)\Steam\userdata\$u\730\local\cfg"
+	Write-Error 'Steam installation not found in default directory'
 }
 
 if($pano -eq 'on'){
-	Copy-Item '.\pano\ancient.webm' -Destination 'C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\panorama\videos'
+	if(Test-Path -Path 'C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo'){
+		Copy-Item '.\pano\ancient.webm' -Destination 'C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\panorama\videos'
+	}
+	else{
+		Write-Error 'CSGO installation not found in default directory'
+	}
 }
